ee7a67d488deb19a94111ab19bc0285e6a432955
Richard Smith-Unna <richardsmith404@gmail.com>
2016-04-20 17:48:31 +0100

    Skip dat join if datasource file exists (needs fixup)

--- app/lib/metadataSource.js
+++ app/lib/metadataSource.js
@@ -1,5 +1,5 @@
-
 var path = require('path')
+var exists = require('path-exists').sync
 
 function MetadataSource(source, datadir) {
   if (!(this instanceof MetadataSource)) {
@@ -11,10 +11,16 @@ function MetadataSource(source, datadir) {
 }
 
 MetadataSource.prototype.download = function(cb) {
+  var self = this
   var opts = { location: path.join(this.datadir, this.dir) }
 
+  if (exists(path.join(opts.location, this.filename))) {
+    console.log('metadata source file exists - skipping download')
+    cb(null, self)
+  }
+
   require('dat-manager')(opts).start(this.datHash, opts, (err, dat) => {
-    cb(err, this)
+    cb(err, self)
   })
 }
 
