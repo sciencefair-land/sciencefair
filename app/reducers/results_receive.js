2946dfb454ac9e8e1f0060d6fd5a0feb9c00fe63
Richard Smith-Unna <richardsmith404@gmail.com>
2016-08-07 19:59:47 +0300

    Preserve selected paper if results change

--- app/reducers/results_receive.js
+++ app/reducers/results_receive.js
@@ -1,11 +1,23 @@
 const uniqBy = require('lodash/uniqBy')
 
 module.exports = (data, state) => {
+  var selected = null
+  if (state.selectedpaper) {
+    state.results[state.selectedpaper]
+  }
+
   const results = uniqBy(state.results.concat(data.hits), (result) => {
     return result.document.identifier[0].id
   })
-  // TODO: intelligent merging of results based on TF-IDF score
-  return {
+
+  const update = {
     results: results
   }
+
+  if (selected) {
+    update.selectedpaper = results.indexOf(selected)
+  }
+
+  // TODO: intelligent merging of results based on TF-IDF score
+  return update
 }
