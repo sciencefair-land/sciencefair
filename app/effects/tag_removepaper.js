e649dcc5d3084cd90200963268a7f90d4ab90d1e
Richard Smith-Unna <richardsmith404@gmail.com>
2016-08-12 15:01:29 +0300

    Fix use of splice

--- app/effects/tag_removepaper.js
+++ app/effects/tag_removepaper.js
@@ -2,7 +2,8 @@ module.exports = (data, state, send, done) => {
   const papers = state.tags.tags[data.tag]
 
   const removeidx = papers.indexOf(data.paper)
-  const newPapers = (removeidx > -1) ? papers.splice(removeidx, 1) : papers
+  const newPapers = papers.slice()
+  if (removeidx > -1) papers.splice(removeidx, 1)
 
   send('tag_replace', { tag: data.tag, papers: newPapers }, done)
 }
