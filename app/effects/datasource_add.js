f15d9a8b279600d3c8e8df669770dff7dec8673f
Richard Smith-Unna <richardsmith404@gmail.com>
2016-10-15 15:33:24 +0300

    Fix error msg about datasource key length

--- app/effects/datasource_add.js
+++ app/effects/datasource_add.js
@@ -1,9 +1,9 @@
 const datasource = require('../../lib/datasource')
 
 module.exports = (data, state, send, done) => {
-  if (data.key.length !== 64) {
-    return done(new Error('datasource keys must be 32 characters long'))
-  }
+    if (data.key.length !== 64) {
+      return done(new Error('datasource keys must be 64 characters long'))
+    }
   datasource.fetch(data.key, (err, ds) => {
     if (err) return done(err)
 
