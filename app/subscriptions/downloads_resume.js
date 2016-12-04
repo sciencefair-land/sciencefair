eb3d5bcc7acfa460325fa8102c2f9c1823e209c3
Rik Smith-Unna <rik@fathomlabs.io>
2016-12-04 22:22:13 +0300

    Notify on startup when downloads are restarted

--- app/subscriptions/downloads_resume.js
+++ app/subscriptions/downloads_resume.js
@@ -5,14 +5,26 @@ const localcollection = require('../../lib/localcollection')
 const getpaper = require('../../lib/getpaper')
 
 const restartdownloads = cb => localcollection(
-  (err, db) => db.docstore
-    .createReadStream()
-    .on('data', data => {
-      const paper = getpaper(data.value)
-      if (!paper.progress || paper.progress < 100) paper.download(() => {})
-    })
-    .on('end', cb)
-    .on('error', cb)
+  (err, db) => {
+    let n = 0
+    db.docstore
+      .createReadStream()
+      .on('data', data => {
+        const paper = getpaper(data.value)
+        if (!paper.progress || paper.progress < 100) {
+          n++
+          paper.download(() => {})
+        }
+      })
+      .on('end', () => cb(null, n))
+      .on('error', cb)
+  }
 )
 
-module.exports = (send, done) => restartdownloads(done)
+module.exports = (send, done) => restartdownloads((err, n) => {
+  if (err) return done(err)
+  send('note_add', {
+    title: 'Restoring downloads',
+    message: `${n} partially completed downloads have been restarted`
+  }, done)
+})
