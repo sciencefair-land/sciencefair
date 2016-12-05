7ece802ef8f6158e7487d320f5a03d61258d984e
Rik Smith-Unna <rik@fathomlabs.io>
2016-12-05 15:29:03 +0300

    Notify user if downloads have to be queued

--- app/effects/download_add.js
+++ app/effects/download_add.js
@@ -1,8 +1,16 @@
-const groupBy = require('lodash/groupBy')
 const datasource = require('../../lib/getdatasource')
+const all = require('lodash/every')
 
 module.exports = (data, state, send, done) => {
-  const alldone = require('../../lib/alldone')(data.length + 1, done)
-  data.forEach(d => d.download(alldone))
-  send('tag_add', { tag: '__local' }, alldone)
+  const allready = data.forEach(p => p.ds.articleMetadataSynced())
+  const ntasks = data.length + allready ? 1 : 2
+  const alldone = require('../../lib/alldone')(ntasks, done)
+  data.forEach(p => p.download(alldone))
+  if (!allready) {
+    send('note_add', {
+      title: `Download${data.length > 1 ? 's' : ''} queued`,
+      message: 'Datasource is still syncing metadata, downloads queued'
+    }, alldone)
+  }
+  send('tag_add', { tag: '__local', paper: data }, alldone)
 }
