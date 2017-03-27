4fa188870f424fb5c60aa2aa96d8d06adcbd849f
Rik Smith-Unna <rik@fathomlabs.io>
2017-03-27 04:41:20 +0300

    Only load updater at runtime - catch error

--- app/client/lib/updater.js
+++ app/client/lib/updater.js
@@ -1,24 +1,27 @@
-const { app, dialog } = require('electron')
-const { autoUpdater } = require('electron-updater')
-
 module.exports = () => {
-	// Log whats happening
-	const log = require('electron-log')
-	log.transports.file.level = 'info'
-	autoUpdater.logger = log
+  try {
+    const { app, dialog } = require('electron')
+    const { autoUpdater } = require('electron-updater')
+    // Log whats happening
+    const log = require('electron-log')
+    log.transports.file.level = 'info'
+    autoUpdater.logger = log
 
-	autoUpdater.on('update-downloaded', (event, info) => {
-		dialog.showMessageBox({
-			type: 'question',
-			buttons: ['Install and Relaunch', 'Later'],
-			defaultId: 0,
-			message: 'A new version of ScienceFair has been downloaded',
-			detail: 'It will be installed the next time you restart the application'
-		}, response => {
-			if (response === 0) {
-				setTimeout(() => autoUpdater.quitAndInstall(), 1)
-			}
-		})
-	})
-	autoUpdater.checkForUpdates()
+    autoUpdater.on('update-downloaded', (event, info) => {
+      dialog.showMessageBox({
+        type: 'question',
+        buttons: ['Install and Relaunch', 'Later'],
+        defaultId: 0,
+        message: 'A new version of ScienceFair has been downloaded',
+        detail: 'It will be installed the next time you restart the application'
+      }, response => {
+        if (response === 0) {
+          setTimeout(() => autoUpdater.quitAndInstall(), 1)
+        }
+      })
+    })
+    autoUpdater.checkForUpdates()
+  } catch (e) {
+    console.error('Could not load auto-updater (perhaps app is not installed)', e)
+  }
 }
