5037dabcc2187512dd0f8dd28783b566a4bd9d62
Rik Smith-Unna <rik@fathomlabs.io>
2017-05-05 16:30:24 +0300

    Typo in makesafe -_- (#59)

--- app/client/lib/imgpath.js
+++ app/client/lib/imgpath.js
@@ -3,6 +3,6 @@ const win = require('os').platform() === 'win32'
 
 const imgdir = path.join(__dirname, '..', 'images')
 
-makesage = str => win ? str.replace('\\', '\\\\') : str
+const makesafe = str => win ? str.replace('\\', '\\\\') : str
 
 module.exports = img => makesafe(path.join(imgdir, img))
