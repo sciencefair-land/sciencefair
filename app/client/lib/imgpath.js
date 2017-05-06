790c0d6fd5599670f41aae5b1da2858963aa713a
Rik Smith-Unna <rik@fathomlabs.io>
2017-05-06 04:39:18 +0300

    Fix imgpath again - this time replace all path separators

--- app/client/lib/imgpath.js
+++ app/client/lib/imgpath.js
@@ -3,6 +3,6 @@ const win = require('os').platform() === 'win32'
 
 const imgdir = path.join(__dirname, '..', 'images')
 
-const makesafe = str => win ? str.replace('\\', '\\\\') : str
+const makesafe = str => win ? str.replace(/\\/g, '\\\\') : str
 
 module.exports = img => makesafe(path.join(imgdir, img))
