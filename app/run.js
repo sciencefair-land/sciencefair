f8e65f00ec0ed2f29a9a8dc7cfa54d513e66aa10
Rik Smith-Unna <rik@fathomlabs.io>
2017-06-23 21:05:08 +0300

    enable electrom-debug in release builds

--- app/run.js
+++ app/run.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow, protocol } = require('electron')
 
-if (process.env['SCIENCEFAIR_DEVMODE']) require('electron-debug')({ enable: true })
+require('electron-debug')({ enable: true })
 
 const path = require('path')
 const open = require('opn')
