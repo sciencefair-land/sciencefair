f2882fef3250a2888ba7902c5ecd6118df3dae6a
Rik Smith-Unna <rik@fathomlabs.io>
2017-06-24 00:11:42 +0300

    fix windows icon path generation (closes #83)

--- app/client/lib/imgpath.js
+++ app/client/lib/imgpath.js
@@ -1,6 +1,1 @@
-const path = require('path')
-const win = require('os').platform() === 'win32'
-
-const imgdir = path.join('.', 'images')
-
-module.exports = img => path.join(imgdir, img)
+module.exports = img => `./images/${img}`
