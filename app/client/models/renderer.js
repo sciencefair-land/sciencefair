1a7378a77535af03646066ea9ce275b73101afb1
Rik Smith-Unna <rik@fathomlabs.io>
2017-06-26 00:38:39 +0300

    Run throttled render on leading and trailing edge

--- app/client/models/renderer.js
+++ app/client/models/renderer.js
@@ -2,7 +2,7 @@ const throttle = require('lodash/throttle')
 
 module.exports = (state, bus) => {
   const render = () => bus.emit('render')
-  const queue = throttle(render, 250)
+  const queue = throttle(render, 250, { leading: true, trailing: true })
 
   const mayberender = () => {
     if (state.reading) {
