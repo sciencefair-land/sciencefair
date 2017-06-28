04a411afd200bba3b228f9619f4ebdfa35a62481
Rik Smith-Unna <rik@fathomlabs.io>
2017-06-29 02:46:53 +0300

    Do not navigate until DOMContentLoaded

--- app/client/models/main.js
+++ app/client/models/main.js
@@ -3,9 +3,22 @@ const C = require('../lib/constants')
 module.exports = (state, bus) => {
   state.initialising = false
 
-  bus.on('initialising:start', () => {
+  let domloaded = false
+  let initstarted = true
+
+  const start = () => {
     state.initialising = true
     bus.emit('pushState', '#initial')
+  }
+
+  bus.on('initialising:start', () => {
+    initstarted = true
+    if (domloaded) return start()
+  })
+
+  bus.on('DOMContentLoaded', () => {
+    domloaded = true
+    if (initstarted) return start()
   })
 
   bus.on('initialising:stop', () => {
