78a7114fcc2fd7b619fcf17c2ae9502dc499a827
Rik Smith-Unna <rik@fathomlabs.io>
2017-07-02 09:39:10 +0300

    debounce, not throttle, render

--- app/client/models/renderer.js
+++ app/client/models/renderer.js
@@ -1,8 +1,8 @@
-const throttle = require('lodash/throttle')
+const debounce = require('lodash/debounce')
 
 module.exports = (state, bus) => {
   const render = () => bus.emit('render')
-  const queue = throttle(render, 250, { leading: true, trailing: true })
+  const queue = debounce(render, 250, { leading: true, trailing: true })
   let frozen = false
 
   const freeze = () => frozen = true
