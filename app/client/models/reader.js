831cb55d28d11168b18dff3affb82fb324a2d7d3
Rik Smith-Unna <rik@fathomlabs.io>
2017-07-02 04:30:14 +0300

    Generalise renderer freezing

--- app/client/models/reader.js
+++ app/client/models/reader.js
@@ -2,12 +2,14 @@ module.exports = (state, bus) => {
   state.reading = null
 
   bus.on('reader:read', paper => {
+    bus.emit('renderer:freeze')
     if (state.reading) return
     state.reading = paper
     bus.emit('pushState', '#reader')
   })
 
   bus.on('reader:quit', () => {
+    bus.emit('renderer:unfreeze')
     state.reading = null
     bus.emit('pushState', '#home')
   })
--- app/client/models/renderer.js
+++ app/client/models/renderer.js
@@ -3,14 +3,20 @@ const throttle = require('lodash/throttle')
 module.exports = (state, bus) => {
   const render = () => bus.emit('render')
   const queue = throttle(render, 250, { leading: true, trailing: true })
+  let frozen = false
+
+  const freeze = () => frozen = true
+  const unfreeze = () => frozen = false
 
   const mayberender = () => {
-    if (state.reading) {
+    if (frozen) {
       return
     } else {
       queue()
     }
   }
 
+  bus.on('renderer:freeze', freeze)
+  bus.on('renderer:unfreeze', unfreeze)
   bus.on('renderer:render', mayberender)
 }
