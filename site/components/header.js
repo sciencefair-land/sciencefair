c623fa7e15ca6bee0d8a5ecc325562dd2f824ae5
Rik Smith-Unna <rik@fathomlabs.io>
2018-05-30 21:32:24 +0100

    Update header

--- site/components/header.js
+++ site/components/header.js
@@ -5,8 +5,16 @@ const intro = require('./intro')
 
 const tagline = () => html`
 
+<h2 class="w-100 dark-gray f2 tc">
+  A desktop app for discovering, reading, and organizing science.
+</h2>
+
+`
+
+const subline = () => html`
+
 <h3 class="w-100 dark-gray f3 tc">
-  A desktop app for discovering, reading, and organizing scientific literature. It's not like anything you've used before - ScienceFair is the global science library of the future.
+  It's not like anything you've used before - ScienceFair showcases the future of scientific literature.
 </h3>
 
 `
@@ -16,6 +24,7 @@ module.exports = () => html`
 <div class="header-container w-100 pa1 pa3-m ph5-ns pv4-ns tc center">
   <img class="w-80-ns w-100" src="assets/header.svg" alt="header-image"/>
   ${h1({ content: tagline(), dark: true })}
+  ${subline()}
   ${menu()}
   ${intro()}
 </div>
