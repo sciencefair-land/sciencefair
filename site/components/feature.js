a0b9df0753e781aec41f4bf70007950fff8b434b
Rik Smith-Unna <rik@fathomlabs.io>
2018-05-31 08:12:55 +0100

    Fixup responsive layout

--- site/components/feature.js
+++ site/components/feature.js
@@ -2,7 +2,7 @@ const html = require('choo/html')
 
 module.exports = opts => html`
 
-<li class="center w-100 w-50-ns fl">
+<li class="center w-100 w-50-l fl">
   <div class="center w-90 lh-copy bg-white border-box pa3 ma3 mid-gray ba br3">
     <div class="tc">
       <img class="fl w-100" src="${opts.img}" />
